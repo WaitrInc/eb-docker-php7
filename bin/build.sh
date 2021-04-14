16d68ffa76609db2d484db715d2e00404e51e576
Cory Collier <cory.collier@waitrapp.com>
2021-04-14 09:59:55 -0400

    adding sanity checks to build and deploy scripts

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# first, be sure that an environment file exists
+[ -f .env ] || {
+    "An environment file [.env], with variables [AWS_ACCOUNT_ID] is required"
+    exit 1
+}
+
+# source the environment file
 . .env
 
-docker build --no-cache -t php7 .
+# Iterate over required binaries, to ensure the system has all the requirements
+for req in docker git; do
+    [ ! -z "$(which "${req}")" ] || {
+        echo "The required binary [${req}] was not found"
+        exit 1
+    }
+done
+
+# Sanity checks passed. Do the work
+GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+docker build --no-cache -t "php7:${GIT_BRANCH}" .
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
+# first, be sure that an environment file exists
+[ -f .env ] || {
+    "An environment file [.env], with variables [AWS_ACCOUNT_ID] is required"
+    exit 1
+}
+
+# source the environment file
 . .env
 
+# Iterate over required variables, and ensure that they are set.
+for req in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_ACCOUNT_ID; do
+  [ ! -z "${!req}" ] || {
+      echo "The required variable [${req}] is empty"
+      exit 1
+  }
+done
+
+# Iterate over required binaries, to ensure the system has all the requirements
+for req in aws docker git; do
+    [ ! -z "$(which "${req}")" ] || {
+        echo "The required binary [${req}] was not found"
+        exit 1
+    }
+done
+
+# Sanity checks passed. Do the work
+GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com"
-docker tag php7:latest "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/php7:latest"
-docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/php7:latest"
+docker tag "php7:${GIT_BRANCH}" "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/php7:${GIT_BRANCH}"
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/php7:${GIT_BRANCH}"
